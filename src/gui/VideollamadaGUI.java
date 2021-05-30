39514ec1e40370bff5527fdb1259e9f6c4160bf3
juca <garciamjuancarlos14@gmail.com>
2021-05-30 00:47:46 -0500

    Se mejoró la representación visual de la VideollamadaGUI

--- src/gui/VideollamadaGUI.java
+++ src/gui/VideollamadaGUI.java
@@ -21,7 +21,7 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
     private JLabel lblImagenLocal;
     private JLabel lblImagenExterna;
     private JButton btnColgarVllamada;
-    //private JButton btnIniciarVllamada;
+    private JButton btnIniciarVllamada;
     private Webcam webcam;
     private CapturadorVideo capturadorVideo;
     private CapturadorAudio capturadorAudio;
@@ -43,8 +43,6 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
         setLocationRelativeTo(null);
         cargarComponentes();
         agregarEventos();
-        iniciarAudio();
-        iniciarVideo();
     }
 
     private void cargarComponentes() {
@@ -59,26 +57,23 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
         panelGeneral.add(panelVideo,BorderLayout.CENTER);
 
         panelOpciones = new JPanel(new FlowLayout());
-        //btnIniciarVllamada = new JButton("Iniciar videollamada");
+        btnIniciarVllamada = new JButton("Iniciar videollamada");
         btnColgarVllamada = new JButton("Colgar videollamda");
-        //panelOpciones.add(btnIniciarVllamada);
-        panelOpciones.add(btnColgarVllamada);
+        panelOpciones.add(btnIniciarVllamada);
         panelGeneral.add(panelOpciones, BorderLayout.PAGE_END);
     }
 
     private void agregarEventos() {
-        btnColgarVllamada.addActionListener(new ColgarVideollamda(this));
+        btnIniciarVllamada.addActionListener(new EventoLlamada());
+        btnColgarVllamada.addActionListener(new EventoLlamada());
     }
 
     private void iniciarVideo() {
-        if(webcam == null)
-        {
-            webcam = Webcam.getDefault();
-            webcam.setViewSize(new Dimension(320,240));
-            webcam.open();
-            capturadorVideo = new CapturadorVideo();
-            capturadorVideo.start();
-        }
+        webcam = Webcam.getDefault();
+        webcam.setViewSize(new Dimension(320,240));
+        webcam.open();
+        capturadorVideo = new CapturadorVideo();
+        capturadorVideo.start();
     }
 
     private void detenerVideo() {
@@ -86,28 +81,32 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
         webcam.close();
     }
 
+    private void detenerAudio() {
+        capturadorAudio.interrupt();
+        microfono.close();
+        bocinas.close();
+    }
+
     private void iniciarAudio() {
         try {
-            if(formatoAudio == null) {
-                // Se especifica el formato del audio a ser enviado.
-                formatoAudio = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100,
-                        16, 2, 4, 44100, true);
-                // Se abre el microfono del dispositivo
-                microfono = AudioSystem.getTargetDataLine(formatoAudio);
+            // Se especifica el formato del audio a ser enviado.
+            formatoAudio = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100,
+                    16, 2, 4, 44100, true);
+            // Se abre el microfono del dispositivo
+            microfono = AudioSystem.getTargetDataLine(formatoAudio);
 
-                DataLine.Info info = new DataLine.Info(TargetDataLine.class, formatoAudio);
-                microfono = (TargetDataLine) AudioSystem.getLine(info);
-                microfono.open(formatoAudio);
-                microfono.start();
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, formatoAudio);
+            microfono = (TargetDataLine) AudioSystem.getLine(info);
+            microfono.open(formatoAudio);
+            microfono.start();
 
-                // Se abren las bocinas del dispositivo
-                DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, formatoAudio);
-                bocinas = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
-                bocinas.open(formatoAudio);
-                bocinas.start();
-                capturadorAudio = new CapturadorAudio();
-                capturadorAudio.start();
-            }
+            // Se abren las bocinas del dispositivo
+            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, formatoAudio);
+            bocinas = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+            bocinas.open(formatoAudio);
+            bocinas.start();
+            capturadorAudio = new CapturadorAudio();
+            capturadorAudio.start();
         } catch (LineUnavailableException ex) {
             ex.printStackTrace();
         }
@@ -151,11 +150,7 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
                 while (true) {
                     byteOutput.reset();
                     numBytesLeer = microfono.read(bytes, 0, tamanoChunck);
-                    //  bytesRead += numBytesRead;
-                    // write the mic data to a stream for use later
                     byteOutput.write(bytes, 0, numBytesLeer);
-                    // write mic data to stream for immediate playback
-                    //bocinas.write(bytes, 0, numBytesLeer);
                     enviarSampleAudio(byteOutput.toByteArray());
                 }
             } catch (Exception ex) {
@@ -164,17 +159,21 @@ public class VideollamadaGUI extends JFrame implements ImpresoraChat {
         }
     }
 
-    private class ColgarVideollamda implements ActionListener {
-        private VideollamadaGUI ventana;
-
-        public ColgarVideollamda(VideollamadaGUI ventana) {
-            this.ventana = ventana;
-        }
+    private class EventoLlamada implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            ventana.detenerVideo();
-            ventana.dispose();
+            Object origen = e.getSource();
+            if(origen == btnIniciarVllamada) {
+                iniciarAudio();
+                iniciarVideo();
+                panelOpciones.remove(btnIniciarVllamada);
+                panelOpciones.add(btnColgarVllamada);
+            } else if(origen == btnColgarVllamada) {
+                detenerVideo();
+                detenerAudio();
+                dispose();
+            }
         }
     }
 
